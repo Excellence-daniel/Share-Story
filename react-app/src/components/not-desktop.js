7c19f80821569662338fb84fbfd401c6eb64b8a4
Daniel <eoyeniran@tradedept.co>
2020-06-20 17:18:49 +0100

    added router and displayed a different screen for if you open app on desktop browser

---
+++ react-app/src/components/not-desktop.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function NotForDesktop() {
+	return <h2>Not for Desktop, use a mobile browser.</h2>;
+}
---
+++ react-app/src/router.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { SplashScreen } from './pages/splash';
+import Media from 'react-media';
+import { BREAKPOINT_SMALL } from './assets/partials/css-constants';
+import NotForDesktop from './components/not-desktop';
+
+function AppRouter() {
+	return (
+		<Media query={{ maxWidth: BREAKPOINT_SMALL }}>{(matches) => (matches ? <Routes /> : <NotForDesktop />)}</Media>
+	);
+}
+
+const Routes = () => {
+	return (
+		<Router>
+			<Route exact path="/" component={SplashScreen} />
+		</Router>
+	);
+};
+
+export default AppRouter;
