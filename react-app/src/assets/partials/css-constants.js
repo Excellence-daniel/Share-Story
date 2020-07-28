8dceea225c9211b5781f8fbf80a2908a73ba2fbd
Daniel <eoyeniran@tradedept.co>
2020-07-28 20:41:03 +0100

    just the node file

--- react-app/src/assets/partials/css-constants.js
+++
@@ -1,4 +0,0 @@
-export const BREAKPOINT_SMALL = 767;
-export const BREAKPOINT_MED = 1028;
-export const BREAKPOINT_LARGE = 1280;
-export const BREAKPOINT_XLARGE = 1440;
--- react-app/src/components/not-desktop.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function NotForDesktop() {
-	return <h2>Not for Desktop, use a mobile browser.</h2>;
-}
