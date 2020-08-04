15fe58d2622fbe23684a53cfe33bec5918280211
Daniel <eoyeniran@tradedept.co>
2020-08-04 10:07:39 +0100

    set heroku port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = 4030; //we use the process port because the other port might not be available so heroku would provide us with a port that is.
+const port = process.env.PORT || 4030; //we use the process port because the other port might not be available so heroku would provide us with a port that is.
 
 // const authEndpoints = require("./connector/login");
 
