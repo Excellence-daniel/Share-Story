99e36137fa18c5e418bad7d6c7ced461b0bcd209
Daniel <eoyeniran@tradedept.co>
2020-08-04 09:21:23 +0100

    added comment

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.listen(port, () => {
 
 // app.get("/login", authEndpoints.login);
 app.get("/login", async (request, response) => {
+  console.log(request.body, "request body");
   console.log("hey login endpoijnt");
   return response.status(200).send("hey hello");
 });
