e6192c35c3467f480b397138f2f4ea68646a465e
Daniel <eoyeniran@tradedept.co>
2020-08-10 08:29:42 +0100

    added lints and defined routes

---
+++ src/router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import // BrowserRouter as Router,
+// Route,
+// Switch,
+// Redirect,
+'react-router-dom';
+
+const AppRoutes = () => {
+  return <></>;
+};
+
+export default AppRoutes;
